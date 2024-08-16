efd2e52e9cebdeab60694621970a1f61adf26f6f
satish_sinha <satishk.sinha@digitalindia.gov.in>
2024-08-16 23:34:45 +0530

    issue resolved and get user details if user exists

--- backend/app/user_info.py
+++ backend/app/user_info.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, HTTPException, Depends
+from fastapi import APIRouter, HTTPException, Depends, status
 from pydantic import BaseModel, EmailStr
 from app.database import get_mongo_client, MONGO_DB, MONGO_COLLECTION
 
@@ -11,13 +11,22 @@ class ProfileRequest(BaseModel):
 
 @router.post("/get_profile")
 async def get_info(request: ProfileRequest, mongo_client=Depends(get_mongo_client)):
-    return MONGO_COLLECTION
-    # db = mongo_client[MONGO_DB]  # Get the database
-    # users_collection = db[MONGO_COLLECTION]  # Get the collection
-    #
-    # # Check if the user already exists
-    # check_user = users_collection.find_one({"user_email": request.email})
-    # return {"success": True, "message": check_user}
-    # print(check_user)
-    # exit()
-    # if check_user:
+    db = mongo_client[MONGO_DB]  # Get the database
+    users_collection = db[MONGO_COLLECTION]  # Get the collection
+
+    # Check if the user exists
+    user = users_collection.find_one({"user_email": request.email}, {
+        "unique_id": 1,
+        "name": 1,
+        "given_name": 1,
+        "family_name": 1,
+        "gender": 1,
+        "dob": 1,
+        "_id": 0,
+
+    })
+
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
+
+    return {"success": True, "message": "User found", "user_details": user}
