ad2e2682980f82a9e9f503e72717ac1ec8770155
satish_sinha <satishk.sinha@digitalindia.gov.in>
2024-09-05 18:34:16 +0530

    code indentation correction

--- backend/app/client_service.py
+++ backend/app/client_service.py
@@ -19,57 +19,6 @@ ENCRYPTION_KEY = os.getenv("ENCRYPTION_KEY")
 cipher_suite = Fernet(ENCRYPTION_KEY)
 
 
-class ClientServiceListRequest(BaseModel):
-    client_email: EmailStr
-
-
-class ClientServiceAddRequest(BaseModel):
-    client_email: EmailStr
-    app_key: str
-    service_name: str
-    service_domain: str
-    service_uri: str
-
-
-class ClientServiceAppRequest(BaseModel):
-    client_id: str
-
-
-class ClientServiceApproveRequest(BaseModel):
-    client_email: EmailStr
-    client_id: str
-
-
-class LoginRequest(BaseModel):
-    email: str
-    clientId: str
-    transactionId: str
-    origin: str
-
-
-class TokenValidation(BaseModel):
-    app_key: str
-    app_secret: str
-    token: str
-
-
-def hash_password(password: str) -> str:
-    return hashlib.sha256(password.encode()).hexdigest()
-
-
-class RegistrationRequest(BaseModel):
-    user_email: str
-    city_id: str
-    country_id: str
-    dob: str
-    name: str
-    state_id: str
-    user_phone: str
-    clientId: str
-    transactionId: str
-    origin: str
-
-
 # Function to encrypt a given app_key
 @router.post("/encrypt_clientid", tags=["Service Management"])
 def encrypt_app_key(app_key: str) -> str:
@@ -84,8 +33,82 @@ def decrypt_app_key(encrypted_key: str) -> str:
     return decrypted_key.decode("utf-8")
 
 
+class ClientServiceListRequest(BaseModel):
+    client_email: EmailStr
+
+
+# Endpoint to generate and store client_id and client_secret
+@router.post("/generate_client", tags=["Service Management"])
+async def generate_client_id(request: ClientServiceListRequest, mongo_client=Depends(get_mongo_client)):
+
+    db = mongo_client[MONGO_DB]
+    service_collection = db[MONGO_SERVICE_COLLECTION]
+    sso_users_collection = db[MONGO_CLIENT_COLLECTION]
+    sso_client_collection = db[MONGO_COLLECTION]
+
+    if not request.client_email:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Client email is required!")
+
+    # Generate unique app_key and ensure it's not in use
+    while True:
+        combine_data = f"{uuid4()}_{time.time()}_{secrets.token_hex(16)}"
+        appkey_hash = hashlib.sha256(combine_data.encode()).hexdigest()
+        app_key = '-'.join(appkey_hash[i:i + 8] for i in range(0, len(appkey_hash), 8))
+        existing_client = service_collection.find_one({"app_key": app_key})
+        if not existing_client:
+            break
+
+    app_secret = secrets.token_hex(40)
+    created_at = datetime.now().strftime('%d-%m-%Y %H:%M:%S')
+
+    client_data = {
+        "client_email": request.client_email,
+        "app_key": app_key,
+        "app_secret": app_secret,
+        "created_at": created_at
+    }
+
+    # get client's record from user master and prepare to insert in a client collection
+    user = sso_client_collection.find_one({"user_email": request.client_email})
+
+    if not user:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid! Data not found")
+
+    client_rec = {
+        "app_key": app_key,
+        "app_secret": app_secret,
+        "user_email": request.client_email,
+        "passkey": user.get("passkey"),
+        "user_role": "CL-ADMIN",
+        "address": user.get("address"),
+        "city_id": user.get("city_id"),
+        "country_id": user.get("country_id"),
+        "dob": user.get("dob"),
+        "gender": user.get("gender"),
+        "name": user.get("name"),
+        "state_id": user.get("state_id"),
+        "user_phone": user.get("user_phone"),
+        "zip": user.get("zip")
+    }
+    try:
+        service_collection.insert_one(client_data)
+        sso_users_collection.insert_one(client_rec)
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Failed to insert data into the database")
+
+    return {
+        "success": True,
+        "status_code": 200,
+        "message": "Unique App Key generated and inserted successfully!",
+        "app_key": app_key,
+        "app_secret": app_secret,
+        "created_at": created_at
+    }
+
+
 @router.post("/get_service_list", tags=["Service Management"])
 async def get_service_list(request: ClientServiceListRequest, mongo_client=Depends(get_mongo_client)):
+
     db = mongo_client[MONGO_DB]
     service_collection = db[MONGO_SERVICE_COLLECTION]
     sso_users_collection = db[MONGO_COLLECTION]
@@ -98,7 +121,7 @@ async def get_service_list(request: ClientServiceListRequest, mongo_client=Depen
     user = sso_users_collection.find_one({"user_email": requested_email}, {"_id": 0, "user_email": 1, "user_role": 1})
 
     if not user:
-        raise HTTPException(status_code=400, detail="User not found in User Collection")
+        raise HTTPException(status_code=400, detail="Data not found in User Collection")
 
     user_role = user.get("user_role")
 
@@ -133,9 +156,17 @@ async def get_service_list(request: ClientServiceListRequest, mongo_client=Depen
     return {"success": True, "message": "Service found", "service_details": services}
 
 
+class ClientServiceAddRequest(BaseModel):
+    client_email: EmailStr
+    app_key: str
+    service_name: str
+    service_domain: str
+    service_uri: str
+
+
 @router.post("/add_service", tags=["Service Management"])
 async def add_client_service(request: ClientServiceAddRequest, mongo_client=Depends(get_mongo_client)):
-    db = mongo_client[MONGO_DB]  # Get the database
+    db = mongo_client[MONGO_DB]
     service_collection = db[MONGO_SERVICE_COLLECTION]
 
     if not request.client_email:
@@ -155,84 +186,22 @@ async def add_client_service(request: ClientServiceAddRequest, mongo_client=Depe
         "service_uri": request.service_uri,
         "is_approved": 0
     }
-    result = service_collection.update_one({"client_email": request.client_email, "app_key": request.app_key},
-                                           {"$set": service_data})
+
+    result = service_collection.update_one({"client_email": request.client_email, "app_key": request.app_key}, {"$set": service_data})
+
     if result.modified_count == 0:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="No changes were made")
     else:
         return {"success": True, "status_code": 200, "message": "Service registered successfully"}
 
 
-# Endpoint to generate and store client_id and client_secret
-@router.post("/generate_client", tags=["Service Management"])
-async def generate_client_id(request: ClientServiceListRequest, mongo_client=Depends(get_mongo_client)):
-    db = mongo_client[MONGO_DB]
-    service_collection = db[MONGO_SERVICE_COLLECTION]
-    sso_users_collection = db[MONGO_CLIENT_COLLECTION]
-    sso_client_collection = db[MONGO_COLLECTION]
-
-    if not request.client_email:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Client email is required!")
-
-    # Generate unique app_key and ensure it's not in use
-    while True:
-        combine_data = f"{uuid4()}_{time.time()}_{secrets.token_hex(16)}"
-        appkey_hash = hashlib.sha256(combine_data.encode()).hexdigest()
-        app_key = '-'.join(appkey_hash[i:i + 8] for i in range(0, len(appkey_hash), 8))
-        existing_client = service_collection.find_one({"app_key": app_key})
-        if not existing_client:
-            break
-
-    app_secret = secrets.token_hex(40)
-    created_at = datetime.now().strftime('%d-%m-%Y %H:%M:%S')
-
-    client_data = {
-        "client_email": request.client_email,
-        "app_key": app_key,
-        "app_secret": app_secret,
-        "created_at": created_at
-    }
-    # get client's record from user master and prepare to insert in a client collection
-    user = sso_client_collection.find_one({"user_email": request.client_email})
-
-    if not user:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid credentials")
-
-    client_rec = {
-        "app_key": app_key,
-        "app_secret": app_secret,
-        "user_email": request.client_email,
-        "passkey": user.get("passkey"),
-        "user_role": "CL-ADMIN",
-        "address": user.get("address"),
-        "city_id": user.get("city_id"),
-        "country_id": user.get("country_id"),
-        "dob": user.get("dob"),
-        "gender": user.get("gender"),
-        "name": user.get("name"),
-        "state_id": user.get("state_id"),
-        "user_phone": user.get("user_phone"),
-        "zip": user.get("zip")
-    }
-    try:
-        service_collection.insert_one(client_data)
-        sso_users_collection.insert_one(client_rec)
-    except Exception as e:
-        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-                            detail="Failed to insert data into the database")
-
-    return {
-        "success": True,
-        "status_code": 200,
-        "message": "Unique App Key generated and inserted successfully!",
-        "app_key": app_key,
-        "app_secret": app_secret,
-        "created_at": created_at
-    }
+class FetchClientRequest(BaseModel):
+    client_id: str
 
 
 @router.post("/fetch_client", tags=["Service Management"])
-async def fetch_client_detail(request: ClientServiceAppRequest, mongo_client=Depends(get_mongo_client)):
+async def fetch_client_detail(request: FetchClientRequest, mongo_client=Depends(get_mongo_client)):
+
     db = mongo_client[MONGO_DB]
     service_collection = db[MONGO_SERVICE_COLLECTION]
 
@@ -244,7 +213,7 @@ async def fetch_client_detail(request: ClientServiceAppRequest, mongo_client=Dep
 
     # Check and fetch a list of services
     services = service_collection.find_one({"app_key": decrypted_client_id}, {
-        "_id": 0,  # Exclude the MongoDB ObjectID from the results
+        "_id": 0,
         "client_email": 1,
         "service_domain": 1,
         "service_name": 1,
@@ -266,8 +235,14 @@ async def fetch_client_detail(request: ClientServiceAppRequest, mongo_client=Dep
     }
 
 
+class ClientServiceApproveRequest(BaseModel):
+    client_email: EmailStr
+    client_id: str
+
+
 @router.post("/approve_service", tags=["Service Management"])
 async def approve_service_key(request: ClientServiceApproveRequest, mongo_client=Depends(get_mongo_client)):
+
     db = mongo_client[MONGO_DB]
     service_collection = db[MONGO_SERVICE_COLLECTION]
 
@@ -298,14 +273,58 @@ async def approve_service_key(request: ClientServiceApproveRequest, mongo_client
         }
 
 
+class TokenValidation(BaseModel):
+    app_key: str
+    app_secret: str
+    token: str
+
+
+@router.post("/validate_token", tags=["Client Login & Registration"])
+async def validate_token(validation_request: TokenValidation, mongo_client: MongoClient = Depends(get_mongo_client)):
+
+    sso_token_collection = mongo_client[MONGO_DB][MONGO_TOKEN_COLLECTION]
+
+    client = sso_token_collection.find_one(
+        {
+            "app_key": validation_request.app_key,
+            "app_secret": validation_request.app_secret,
+            "id_token": validation_request.token
+        }
+    )
+
+    if not client:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid client - UNKNOWN CLIENT ID")
+
+    request_time = datetime.utcnow()
+    expire_time = client.get("expire_time")
+
+    if request_time > expire_time:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid Token!")
+
+    jwt_token = client.get("jwt_token")
+    if jwt_token is not None:
+        return {
+            "success": True,
+            "status_code": 200,
+            "message": "Transaction successful",
+            "jwt_token": jwt_token
+        }
+    else:
+        return {
+            "success": False,
+            "status_code": 400,
+            "message": "Transaction failed!"
+        }
+
+
 class ClientVerificationValidation(BaseModel):
     client_id: str
     origin: str
 
 
 @router.post("/client_verification", tags=["Client Login & Registration"])
-async def client_verification(client_request: ClientVerificationValidation,
-                              mongo_client: MongoClient = Depends(get_mongo_client)):
+async def client_verification(client_request: ClientVerificationValidation, mongo_client: MongoClient = Depends(get_mongo_client)):
+
     client_service = mongo_client[MONGO_DB][MONGO_SERVICE_COLLECTION]
 
     client = client_service.find_one(
@@ -323,18 +342,25 @@ async def client_verification(client_request: ClientVerificationValidation,
     }
 
 
+class LoginRequest(BaseModel):
+    email: str
+    clientId: str
+    transactionId: str
+    origin: str
+
+
 @router.post("/client_login", tags=["Client Login & Registration"])
 async def client_login(login_request: LoginRequest, mongo_client: MongoClient = Depends(get_mongo_client)):
-    client_service = mongo_client[MONGO_DB][MONGO_SERVICE_COLLECTION]
+
     sso_users_collection = mongo_client[MONGO_DB][MONGO_COLLECTION]
-    client_collection = mongo_client[MONGO_DB][MONGO_CLIENT_COLLECTION]
     token_collection = mongo_client[MONGO_DB][MONGO_TOKEN_COLLECTION]
+    client_service = mongo_client[MONGO_DB][MONGO_SERVICE_COLLECTION]
+    client_collection = mongo_client[MONGO_DB][MONGO_CLIENT_COLLECTION]
 
     client = client_service.find_one({"app_key": login_request.clientId, "is_approved": 1})
 
     if not client:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
-                            detail="Invalid client credentials - UNKNOWN CLIENT ID")
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid client - UNKNOWN CLIENT ID")
 
     txn = login_request.transactionId
     app_secret = client.get("app_secret")
@@ -346,17 +372,13 @@ async def client_login(login_request: LoginRequest, mongo_client: MongoClient = 
     user = client_collection.find_one({"user_email": login_request.email, "app_key": login_request.clientId})
 
     if not user:
-        # raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Invalid credentials - User not found!")
-        return {
-            "success": False,
-            "status_code": status.HTTP_404_NOT_FOUND,
-            "detail": "Invalid credentials - User not found!"
-        }
+        return {"success": False, "status_code": status.HTTP_404_NOT_FOUND, "detail": "Invalid - User not found!"}
 
+    dob = user.get("dob")
     username = user.get("name")
     user_email = login_request.email
-    dob = user.get("dob")
     user_role = user.get("user_role")
+
     # Convert time in IST Format
     clock_time = get_ist_time()
 
@@ -371,7 +393,6 @@ async def client_login(login_request: LoginRequest, mongo_client: MongoClient = 
         "user_email": user_email,
         "dob": dob,
         "exp": int((clock_time + timedelta(minutes=float(os.getenv("JWT_EXPIRATION_MINUTES")))).timestamp())
-        # Token expiry time
     }
 
     # Generate tokens
@@ -426,38 +447,17 @@ async def client_login(login_request: LoginRequest, mongo_client: MongoClient = 
         }
 
 
-@router.post("/validate_token", tags=["Client Login & Registration"])
-async def validate_token(validation_request: TokenValidation, mongo_client: MongoClient = Depends(get_mongo_client)):
-    sso_token_collection = mongo_client[MONGO_DB][MONGO_TOKEN_COLLECTION]
-
-    client = sso_token_collection.find_one(
-        {"app_key": validation_request.app_key, "app_secret": validation_request.app_secret,
-         "id_token": validation_request.token})
-
-    if not client:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
-                            detail="Invalid client credentials - UNKNOWN CLIENT ID")
-
-    request_time = datetime.utcnow()
-    expire_time = client.get("expire_time")
-
-    if request_time > expire_time:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid Token!")
-
-    jwt_token = client.get("jwt_token")
-    if jwt_token is not None:
-        return {
-            "success": True,
-            "status_code": 200,
-            "message": "Transaction successful",
-            "jwt_token": jwt_token
-        }
-    else:
-        return {
-            "success": False,
-            "status_code": 400,
-            "message": "Transaction failed!"
-        }
+class RegistrationRequest(BaseModel):
+    user_email: str
+    city_id: str
+    country_id: str
+    dob: str
+    name: str
+    state_id: str
+    user_phone: str
+    clientId: str
+    transactionId: str
+    origin: str
 
 
 @router.post("/client_registration", tags=["Client Login & Registration"])
@@ -469,8 +469,7 @@ async def client_registration(registration_request: RegistrationRequest, mongo_c
     client = service_collection.find_one({"app_key": registration_request.clientId, "is_approved": 1})
 
     if not client:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
-                            detail="Invalid client credentials - UNKNOWN CLIENT ID")
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid client - UNKNOWN CLIENT ID")
 
     txn = registration_request.transactionId
     service_name = client.get("service_name")
@@ -498,10 +497,11 @@ async def client_registration(registration_request: RegistrationRequest, mongo_c
     if not user.inserted_id:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid credentials")
 
+    dob = client_data['dob']
     username = client_data['name']
     user_email = client_data['user_email']
-    dob = client_data['dob']
     user_role = client_data['user_role']
+
     # Convert time in IST Format
     clock_time = get_ist_time()
 
@@ -516,7 +516,6 @@ async def client_registration(registration_request: RegistrationRequest, mongo_c
         "user_email": user_email,
         "dob": dob,
         "exp": int((clock_time + timedelta(minutes=float(os.getenv("JWT_EXPIRATION_MINUTES")))).timestamp())
-        # Token expiry time
     }
 
     # Generate tokens
@@ -535,6 +534,7 @@ async def client_registration(registration_request: RegistrationRequest, mongo_c
         "auth_txn": txn,
         "auth_mode": "ONEACCESS_AUTH"
     }
+
     id_token = generate_jwt_token(jwt_data)
     jwt_token = generate_jwt_token(jwt_record)
 
